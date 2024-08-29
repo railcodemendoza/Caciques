d5a8d873f1542765c32b329eebeacd174550dbf5
sofilarrea <sofialejandra.larrea@gmail.com>
2024-08-29 10:34:16 -0300

    navbar en nosotros

--- script/main.js
+++ script/main.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             // Calcula la velocidad del incremento
             const increment = target / 50;
-            
+
             if (count < target) {
                 counter.innerText = Math.ceil(count + increment);
                 setTimeout(updateCount, 10); // Ajusta el tiempo para el efecto deseado
@@ -19,4 +19,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
         updateCount();
     });
+});
+window.addEventListener('scroll', function() {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) { // Ajusta este valor según lo necesites
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
 });
